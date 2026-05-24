f8888f98879d39dbcae023fc9fb6d1343c0222a1
Egor Shramko <egorii2001@gmail.com>
2026-05-24 20:16:08 +0700

    Добавил контроллер для получения всех разрешений в системе

---
+++ src/main/java/io/github/egorshramko/booking/controller/PermissionController.java
@@ -0,0 +1,26 @@
+package io.github.egorshramko.booking.controller;
+
+import io.github.egorshramko.booking.dto.PermissionDto;
+import io.github.egorshramko.booking.facade.PermissionServiceFacade;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/permissions")
+public class PermissionController {
+
+    @Autowired
+    private PermissionServiceFacade permissionService;
+
+    @GetMapping
+    public Set<PermissionDto> getAllPermissions() {
+        return permissionService.getAllSystemPermissions();
+    }
+
+}
