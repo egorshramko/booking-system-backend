3af0eaeb2e1f38752fd99efe8dbfb25079b2c13c
Egor Shramko <egorii2001@gmail.com>
2026-06-07 19:21:56 +0700

    Создал endpoint для получения всех возможных разрешений в системе

--- src/main/java/io/github/egorshramko/booking/controller/PermissionController.java
+++ src/main/java/io/github/egorshramko/booking/controller/PermissionController.java
@@ -4,6 +4,7 @@ import io.github.egorshramko.booking.dto.PermissionDto;
 import io.github.egorshramko.booking.facade.PermissionServiceFacade;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class PermissionController {
     @Autowired
     private PermissionServiceFacade permissionService;
 
+    @PreAuthorize("hasAuthority('READ_PERMISSION')")
     @GetMapping
     public Set<PermissionDto> getAllPermissions() {
         return permissionService.getAllSystemPermissions();
